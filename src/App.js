79bc9523265d03f186660b26413a9cf329e772c0
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-02 11:31:33 -0300

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import globalStore from './redux/configureStore';
+import Messages from './components/messages';
+import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={globalStore}>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Wellcome to the messages app</h1>
+          <Routes>
+            <Route path="/" exact element={<Messages />} />
+            <Route path="/home" exact element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
